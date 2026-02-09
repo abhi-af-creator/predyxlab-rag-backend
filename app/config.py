4820be6c3efeab42e83bf25a2b4a41a04d943514
Abhi <abhi@example.com>
2026-02-10 01:05:10 +0530

    Fix PDF Mobile Upload V0.5

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(os.getenv("HOME", "/home")) #Path(__file__).resolve().parent.parent
 
 DATA_DIR = BASE_DIR / "data"
 UPLOAD_DIR = DATA_DIR / "uploads"
