74f3795de59a284b84b72208745e2f91af6600d6
Abhi <abhi@example.com>
2026-02-10 09:57:36 +0530

    Fix PDF Mobile Upload V0.12

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+#from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.health import router as health_router
-from app.api.ingest import router as ingest_router
-from app.api.search import router as search_router
-from app.api.chat import router as chat_router
+#from app.api.health import router as health_router
+#from app.api.ingest import router as ingest_router
+#from app.api.search import router as search_router
+#from app.api.chat import router as chat_router
 #from app.api import search
 
 app = FastAPI(
@@ -13,20 +13,20 @@ app = FastAPI(
 )
 
 # -------------------- CORS --------------------
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["https://orange-moss-08315ef00.2.azurestaticapps.net",
-                   "https://calm-meadow-06bb15200.2.azurestaticapps.net"],   # tighten later
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+#app.add_middleware(
+#    CORSMiddleware,
+#    allow_origins=["https://orange-moss-08315ef00.2.azurestaticapps.net",
+#                   "https://calm-meadow-06bb15200.2.azurestaticapps.net"],   # tighten later
+#    allow_credentials=True,
+#    allow_methods=["*"],
+#    allow_headers=["*"],
+#)
 
 # -------------------- ROUTERS --------------------
-app.include_router(health_router)
-app.include_router(ingest_router)
-app.include_router(search_router)
-app.include_router(chat_router)
+#app.include_router(health_router)
+#app.include_router(ingest_router)
+#app.include_router(search_router)
+#app.include_router(chat_router)
 
 # -------------------- ROOT --------------------
 @app.get("/")
