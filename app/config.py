281bf07d5f75e22038a86ef0a01bf18a82076c5e
Abhi <abhi@example.com>
2026-02-10 11:56:46 +0530

    Fix yml

--- app/config.py
+++ app/config.py
@@ -1,13 +1,12 @@
 import os
 from pathlib import Path
 
-
-BASE_DIR = Path(os.getenv("HOME", "/home")) #Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path("/tmp")  # <-- THIS FIXES STARTUP
 DATA_DIR = BASE_DIR / "data"
 UPLOAD_DIR = DATA_DIR / "uploads"
+FAISS_DIR = DATA_DIR / "faiss"
 
 UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
-EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
-FAISS_DIR = DATA_DIR / "faiss"
 FAISS_DIR.mkdir(parents=True, exist_ok=True)
+
+EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
